26c5e966f1a9581e44b589e68660aa49815f6eec
Theonilah Owali <theonilahtash@gmail.com>
2018-12-31 13:17:34 +0300

    added the base template and loaded bootstrap4 dependency to define our styles

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def welcome(request):
